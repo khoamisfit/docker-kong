f3a2dc4f18fe3945b06b09e0f406dda6d1128131
Khoa Nguyen <vankhoa011@gmail.com>
2016-04-21 13:24:47 +0700

    Support ENV for Kong postgres DB

--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# Setting up ENV for postgres DB
+if [[ -n "${POSTGRES_HOST}" ]]; then
+  cat /etc/kong/kong.yml \
+      | sed -e "s/%POSTGRES_HOST%/${POSTGRES_HOST}/g"
+fi
+
+if [[ -n "${POSTGRES_DB}" ]]; then
+  cat /etc/kong/kong.yml \
+      | sed -e "s/%POSTGRES_DB%/${POSTGRES_DB}/g"
+fi
+
+if [[ -n "${POSTGRES_USER}" ]]; then
+  cat /etc/kong/kong.yml \
+      | sed -e "s/%POSTGRES_USER%/${POSTGRES_USER}/g"
+fi
 
 # Setting up the proper database
 if [ -n "$DATABASE" ]; then
